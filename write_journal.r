c1531f074320d99d6cbc4083059eb22ba71ae8be
arnaud gaboury <arnaud.gaboury@gmail.com>
2025-02-28 10:41:11 +0100

    finalyse everything to get the desired result

--- write_journal.r
+++ write_journal.r
@@ -8,9 +8,12 @@ library('rjson')
 library('dplyr')
 
 binance_credentials(Sys.getenv('BINANCE_KEY'), Sys.getenv('BINANCE_SECRET')) 
-
-
 tokens <- read.table('assets.csv')
+
+# journal au 16 dec
+my_journal_orig <- journal(timestamp = '2024-12-16', instrument = c('AAVE', 'BTC', 'ENA', 'USDT'),
+                      amount = c(835.68, 20.04765, 432616, 0.23190), price = c(387.87, 107078.55, 1.1853, 1))
+
 # Define start and end dates in POSIXct format (UTC)
 start_date <- as.POSIXct("2024-12-16", tz = 'UTC')
 end_date   <- as.POSIXct("2025-02-17", tz = "UTC")
@@ -66,9 +69,41 @@ trade_list <- trade_list %>% mutate(price = round(price, 6))
 trade_list_final <- trade_list %>%
   mutate(executed_qty = ifelse(side == "SELL", -abs(executed_qty), executed_qty))
 
+# transform into a tibble
+trade_list_tb <- as_tibble(trade_list_final)
+
+# we need BTCUSDT price for these dates
+dates <- c('2025-01-15 22:04:45', '2025-01-19 15:07:30', '2025-01-19 20:36:24', 
+           '2025-01-20 02:04:42', '2025-01-20 02:32:15', '2025-01-30 07:00:43',
+           '2025-02-11 18:58:38')
+# return BTC prices for specific dates
+btc_price_date <- sapply(dates, function(dt) get_binance_price("BTCUSDT", dt))
+
+btc_price <- as_tibble(c(rep(1, 20), 99623.99, rep(1, 3), 104619.91, 106170.25, 100821.97, 
+                         101940.72, rep(1,17), 105250.71, rep(1,5), 95469.28, 1, 1))
+
+# join the two tibbles
+joined_tibble <- bind_cols(trade_list_tb, btc_price)
+# price in USDT 
+final_tb <- joined_tibble %>%
+  mutate(mul = price * value) %>%
+  select(-price, -value)
+# rename mul column
+final_tb <- final_tb %>%
+  rename(price = mul)
+
+# change instrument name in first column
+final_tb[[1]] <- ifelse(final_tb[[1]] == "AAVEBTC", "AAVEUSDT", final_tb[[1]])
+final_tb[[1]] <- ifelse(final_tb[[1]] == "SUIBTC", "SUIUSDT", final_tb[[1]])
+final_tb[54, 1] <- 'BTCUSDT'
+final_tb[39, 1] <- 'AAVEUSDT'
+# remove FTM
+final_tb <- final_tb %>% slice(-9)
+final_tb <- final_tb %>% slice(-13)
+
 # write the journal
-my_journal <- journal(timestamp = trade_list_final$time, instrument = trade_list_final$symbol,
-                       amount = trade_list_final$executed_qty, price = trade_list_final$price)
+my_journal <- journal(timestamp = final_tb$time, instrument = final_tb$symbol,
+                       amount = final_tb$executed_qty, price = final_tb$price)
 
 
 
