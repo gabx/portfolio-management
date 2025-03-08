66b99c9b1c0be9c09ea442a982572ef8b6099bec
arnaud gaboury <arnaud.gaboury@gmail.com>
2025-03-08 09:34:52 +0100

    finally the whole journal, including closing prices. is written

--- write_journal.r
+++ write_journal.r
@@ -105,18 +105,25 @@ trade_list_tb <- trade_list_tb %>%
 
 # fetch price 
 get_price <- function(my_token, my_time) {
-  klines <- binance_klines(my_token, start_time = my_time, end_time = my_time)
+  klines <- binance_klines(my_token, interval = '1h', start_time = as.Date(my_time), end_time = as.Date(my_time) + 1)
+}
+
+get_btc_price <- function(my_time) {
+  klines <- binance_klines("BTCUSDT", interval = '1h', start_time = as.Date(my_time), end_time = as.Date(my_time) + 1)
+  return(as.numeric(klines$close[[18]]))
 }
 
 
 # fetch btcusdt price
 # Function to get btc  price at 18:00 
-get_btc_price <- function(my_time) {
-  # my_time_ms <- as.integer(as.numeric(my_time) * 1000)
-  klines <- binance_klines("BTCUSDT", interval = '1h', start_time = as.Date(my_time), end_time = as.Date(my_time) + 1)
-  return(as.numeric(klines$close[[18]]))
-}
 
+# return BTC prices for specific dates
+btc_dated_price <- get_price('2025-02-21') # example2025-02-11 
+
+
+# fetch btcusdt price
+
+ 
 
 new.column <- mapply(
   function(symbol, mytime) {
@@ -136,21 +143,6 @@ btc_dates <- trade_list_tb %>%
   filter(str_detect(symbol, "BTC$")) %>%
   pull(time)
            
-# return BTC prices for specific dates
-btc_dated_price <- get_price('2025-02-21') # example2025-02-11 
-
-get_price <- function(my_token, my_time) {
-  klines <- binance_klines(my_token, start_time = as.Date(my_time), end_time = as.Date(my_time) + 1)
-  #return(klines[[5]])
-}
-
-# fetch btcusdt price
-get_btc_price <- function(my_time) {
-  # my_time_ms <- as.integer(as.numeric(my_time) * 1000)
-  klines <- binance_klines("BTCUSDT", interval = '1h', start_time = as.Date(my_time), end_time = as.Date(my_time) + 1)
-  return(as.numeric(klines$close[[18]]))
-}
-
 
 btc_price <- as_tibble(c(rep(1, 20), 99623.99, rep(1, 3),104567.99, 104619.91, 106170.25, 100241.60, 100821.97, 
                          101940.72, rep(1,9), 101841, rep(1,8), 103650, rep(1,5), 97332,  rep(1, 4), 98217, rep(1, 6) ))
@@ -186,6 +178,7 @@ journal_tb   <- journal_tb   %>%
 # 2025-01-19 15:05:23 AAVEBTC BUY BTC 0.003020 @ 104567
 # 2025-01-15 22:04:45 AAVEBTC SELL BTC 2.66999289 @ 99623
 
+# BTCUSDT sold/bought when trading token/BTC
 my_BTC <- tibble(
   timestamp = as.POSIXct(c('2025-01-15 22:04:45', '2025-01-19 15:05:23', '2025-01-19 15:07:30', '2025-01-19 20:36:24',
                            '2025-01-20 01:31:45', '2025-01-20 02:04:42', '2025-01-20 02:32:15', '2025-01-28 11:19:10',
@@ -198,19 +191,60 @@ my_BTC <- tibble(
   price = c(99623, 104567, 104619, 106170, 100241, 100821, 101940, 102814, 105250, 95469,105375))
 
 # join journal_tb with my_BTC and original ptf
-my_journal_tb <- bind_rows(journal_tb, my_BTC, my_journal_orig)
+my_journal_tb <- bind_rows(journal_tb, my_BTC)
 
 # order by timestamp
 my_journal_tb <- my_journal_tb  %>% arrange(timestamp)
 
+ my_journal <- journal(
+   timestamp = my_journal_tb$timestamp,
+   instrument = my_journal_tb$instrument,
+   amount = my_journal_tb$amount,
+   price = my_journal_tb$price
+ )
  
+ # Change column order 
+ my_journal_tb <- my_journal_tb %>%
+   select(amount, price, timestamp, instrument) 
 # write the journal
 my_journal <- as.journal(my_journal_tb)
 
+## create a tibble for last trading day of journal ##
+# list elements
+my_position <- position(my_journal)
+token <- instrument(my_position)
+# as a list
+token_ls <- as.list(token)
+# pass the list to get_price()
+fun <- function(x) get_price(x) # broken
+get_AAVE <- 211.85
+get_BTC <- 88628.01
+get_ENA <- 0.3797
+get_ETH <- 2191.2
+get_FTM <- NULL
+get_SUI <- 2.8162
+get_SUSDT <- 0.5451
+
+my_ptf_instrument <- instrument(my_position)
+my_ptf_price <- c(211.85, 88628.01, 0.3797, 2191.2, 0, 2.8162, 0.5451)
+my_ptf_amount <- c(0, 13.61925879, 419796.64, 0, 0, 107599, 323689.3)
+my_ptf_timestamp <- rep(as.POSIXct('2024-12-16 18:00:00', format = "%Y-%m-%d %H:%M:%S", tz = "UTC"), 7)
+
+# get closing price
+get_AAVE <- get_price('AAVEUSDT', '2025-03-06')
 
 
+# end_elements <- unique(as.list(my_journal_tb$instrument))
+# get elements
+end_elements <- my_journal_tb[,4] %>%
+  distinct(instrument)
 
+my_journal_end <- journal(instrument = my_ptf_instrument, 
+  timestamp = my_ptf_timestamp, 
+  amount = my_ptf_amount, 
+  price = my_ptf_price)
 
+my_journal_all <- c(my_journal, my_journal_end)
 
 ############# NICE FORMATS ###################################################
 # Print the journal in nice formats
