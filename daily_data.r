51f54be07596329952b3a147dbc6f802cbd924aa
arnaud gaboury <arnaud.gaboury@gmail.com>
2025-03-12 18:07:40 +0100

    work in progress

--- daily_data.r
+++ daily_data.r
@@ -13,22 +13,22 @@ binance_credentials(Sys.getenv('BINANCE_KEY'), Sys.getenv('BINANCE_SECRET'))
 tokens <- read.table('assets.csv')
 
 # replace BTC with USDT at the end of the token names
-tokens_usdt <- tokens%>%
+tokens_usdt <- tokens %>%
   mutate(V1 = sub("BTC$|USDC", "USDT", V1))
 # remove duplicate
-tokens_usdt <- token_usdt %>%
+tokens_usdt <- tokens_usdt %>%
   distinct()
 # make a list
 tokens_usdt_ls <- as.list(tokens_usdt)
 
 # Define start and end dates in POSIXct format (UTC)
-start_date <- as.POSIXct("2024-12-16", tz = 'UTC')
+start_date <- as.POSIXct("2024-12-17", tz = 'UTC') 
 end_date   <- as.POSIXct("2025-03-12", tz = "UTC")
 date_seq <- seq(start_date, end_date, by = "day")
 
 # get daily close for our token
 # !! pour l'écriture du code !!
-sel_tok <- token_usdt[1:2, ]
+# sel_tok <- token_usdt[1:2, ]
 
 get_price <- function(my_token, start_date, end_date) {
   klines <- binance_klines(my_token, interval = '1h', start_time = as.Date(start_date))
@@ -41,19 +41,23 @@ keep_close <- function(df) {
     filter(format(open_time, "%H:%M:%S") == "06:00:00") %>%
     select(open_time, close)
 }
-df_list_close <- lapply(daily_prices, keep_close)
 
-# names each elements of the list
+
 # this is the final list with daily closes for each token on the period
-names(df_list_close) <- tokens_usdt_ls$V1
+df_list_close <- lapply(daily_prices, keep_close)
+# combine data frames of the list in one df by the common column, open_time
+df_combined <- reduce(df_list_close, full_join, by = "open_time")
+# name columns
+colnames(df_combined)[-1] <- tokens_usdt_ls$V1
+
 
 
 
 # daily_prices <- map_dfr(date_seq, ~ get_price(c('BTCUSDT', 'SUIUSDT'), .x))
 # remove duplicates
-daily_prices_unique <- token_daily %>%
-  distinct(open_time, .keep_all = TRUE)
-daily_prices_unique <- map(token_daily, ~ distinct(.x, open_time, .keep_all = TRUE))
+# daily_prices_unique <- token_daily %>%
+#   distinct(open_time, .keep_all = TRUE)
+# daily_prices_unique <- map(token_daily, ~ distinct(.x, open_time, .keep_all = TRUE))
 # keep price @ 06:00 each day
 daily_prices_close <- daily_prices_unique %>%
   filter(format(open_time, "%H:%M:%S") == "06:00:00") %>%
