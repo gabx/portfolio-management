c9cd5963b582b436cedaa1115ea6660fcd59b88e
arnaud gaboury <arnaud.gaboury@gmail.com>
2025-03-30 23:24:59 +0200

    add code from another script

--- daily_portfolio_valuation.r
+++ daily_portfolio_valuation.r
@@ -1,6 +1,40 @@
 
 
+###############################################################################
+# compute daily portfolio valuation
 
+# return token price at specific time
+trade_list_value <- trade_list %>%
+  mutate(token_valuation = map2_dbl(symbol, time, get_token_price))
+
+
+latest_row <- trade_list %>%
+  slice(which.max(time))
+valuation_time <- latest_row[[1]]
+
+# complete the dates
+trade_list_final <- trade_list_final %>%
+  mutate(date = as.Date(time))
+
+full_dates <- tibble(date = seq(min(trade_list_final$date), max(trade_list_final$date), by = "day"))
+# Complete the data with one row per day
+completed_tb <- trade_list_final %>%
+  right_join(full_dates, by = "date") %>%
+  arrange(date)
+
+# Set timestamp to 20:00:00 for missing timestamps
+completed_tb <- completed_tb %>%
+  mutate(
+    time = if_else(
+      is.na(time),
+      as.POSIXct(paste(date, "18:00:00"), tz = "UTC"),
+      time
+    )
+  )
+
+# Reorder or clean up if needed
+completed_tb <- completed_tb %>%
+  select(-date)  # Remove helper date column if not needed
 
 ##############################################
 ## to work with PerformanceAnalytics ##
