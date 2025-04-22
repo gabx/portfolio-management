fa7ef730f6f08cba0cb2182c3fcd77caa30bafee
arnaud gaboury <arnaud.gaboury@gmail.com>
2025-04-22 23:15:46 +0000

    got a much better solution to handle the 3 day missing data for SUSDC

--- get_daily_close.r
+++ get_daily_close.r
@@ -29,7 +29,7 @@ token_usdc <- unnest(token_usdc, cols = V1)
 # we need to remove FTM from 2025-01-13 (i = 3) as it doesn't exist anymore and
 # split an error and remove SUSDC as it exists much later
 token_usdc <- token_usdc %>% 
-  filter(!V1 %in% c("FTMUSDC", 'SUSDC'))
+  filter(!V1 %in% c("FTMUSDC"))
 
 # function to get prices
 get_price <- function(my_token, start_date) {
@@ -77,70 +77,13 @@ token_daily_close <- token_daily_close %>%
 token_daily_close_final <- token_daily_close %>%
   pivot_wider(names_from = token, values_from = close)
 
-# we shall put FTMUSDC & SUSDC in our token list
-token_usdc <- token_usdc %>%
-  add_row(V1 = 'SUSDC')
+# replace NA with 0.6979 price for SUSDC
+all_token_daily_close_final <- token_daily_close_final %>%
+  mutate(SUSDC = if_else(is.na(SUSDC), 0.6979, SUSDC))
 
 
-############## add data for FTM and S ###################################
-#### FTM ######
-# we nned prices from 12-16 till 2025-01-15 include
-ftm1 <- get_price('FTMUSDC', '2024-12-16')
-ftm2 <- get_price('FTMUSDC', '2025-01-05') # ends 01-13 03:00. Thus we need NA till
-# 01-15 
-ftm <- as_tibble(bind_rows(ftm1, ftm2))
-# remove duplicate
-ftm <- ftm %>%
-  distinct()
-# change FTMUSDC to SUSDC
-ftm_to_s <- ftm %>%
-  mutate(symbol = if_else(symbol == "FTMUSDC", "SUSDC", symbol))
-
-# add some rows with NA from 01-13 03:00 to 01-16 08:00
-add_missing_rows <- function(df, last_open, until_open) {
-  # Crée une séquence de temps horaires
-  new_open_times <- seq(from = as.POSIXct(last_open, tz = "UTC"),
-                        to   = as.POSIXct(until_open, tz = "UTC"),
-                        by   = "1 hour")
-  # Calcule les close_time
-  new_close_times <- new_open_times + minutes(59) + seconds(59)
-  # Récupère les noms de colonnes
-  cols <- names(df)
-  # Crée une tibble vide avec les colonnes attendues
-  na_part <- tibble(open_time  = new_open_times,
-                    high       = NA_real_,
-                    low        = NA_real_,
-                    close      = NA_real_,
-                    open       = NA_real_,
-                    volume     = NA_real_,
-                    close_time = new_close_times,
-                    quote_asset_volume          = NA_real_,
-                    trades                      = NA_integer_,
-                    taker_buy_base_asset_volume = NA_real_,
-                    taker_buy_quote_asset_volume= NA_real_,
-                    symbol     = "SUSDC")
-  # Réordonne les colonnes comme dans df
-  na_part <- na_part[, cols]
-  # Combine les deux
-  bind_rows(df, na_part)
-}
-
-ftm_to_s_extended <- add_missing_rows(ftm, last_open = "2025-01-13 03:00:00", until_open = "2025-01-16 08:00:00")
-
-# select 23:59:59 and specific columns
-s_close <- ftm_to_s_extended %>%
-  filter(format(close_time, "%H:%M:%S") == "23:59:59") %>%
-  select(symbol, close_time, close) %>%
-  rename(token = symbol)
-# put token as column names
-s_daily_close_final <- s_close %>%
-  pivot_wider(names_from = token, values_from = close)
-
-##### FINALLY ##########
-# join s closing prices with other otken
-all_token_daily_close_final <- left_join(s_daily_close_final, token_daily_close_final,  by = "close_time")
-# replace NA in SUSDC column by values from s_daily_close_final
-token_daily_close_final <- rows_update(token_daily_close_final, s_daily_close_final, by = "close_time", unmatched = 'ignore')
+# !! Dirty fix: remove LINK as not trade on it and it gives error 
+token_daily_close_final <- token_daily_close_final %>% select(-LINKUSDC)
 
 
 
