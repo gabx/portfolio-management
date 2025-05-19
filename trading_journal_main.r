17ecea67888dcb0747e0ff3b7a636146401016cd
arnaud gaboury <arnaud.gaboury@gmail.com>
2025-05-19 16:43:15 +0200

    more steps for the main script

--- trading_journal_main.r
+++ trading_journal_main.r
@@ -4,5 +4,13 @@
 setwd('/development/language/r/portfolio-management')
 
 # load variables, package, settings
-# fetch trades as lists
+# fetch trades as lists: trade_ls, trade_ls2
 source('environment_variables.r')
+
+# fetch all trades
+# result: all_trade_final
+source('get_all_trade.r')
+
+# get daily closing prices @ 00:59:59
+# result: token_daily_close_final
+source('get_daily_closes.r')
