7deda6299262d39b0a2a3c4e0fe51613e6a80cbf
arnaud gaboury <arnaud.gaboury@gmail.com>
2025-05-19 15:50:42 +0200

    the main script to run the whole projetc

---
+++ trading_journal_main.r
@@ -0,0 +1,8 @@
+
+
+# chosse the correct working directory
+setwd('/development/language/r/portfolio-management')
+
+# load variables, package, settings
+# fetch trades as lists
+source('environment_variables.r')
