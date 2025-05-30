d8633796ac3430bd2201e3ff64758df343587cf8
arnaud gaboury <arnaud.gaboury@gmail.com>
2025-05-30 22:35:53 +0200

    use base pipe |> rather than magrittr's %>%

--- valuation_table.r
+++ valuation_table.r
@@ -11,9 +11,9 @@
 table_token <- colnames(token_daily_close_final)[-1]
 table_token_tb <- tibble(table_token)
 # test if both are same
-token_usdc <- token_usdc %>%
+token_usdc <- token_usdc |>
       arrange(token_usdc)
-table_token_tb <- table_token_tb %>%
+table_token_tb <- table_token_tb |>
      arrange(table_token_tb)
 # test if equal
 if (!all(table_token_tb == token_usdc)) {
@@ -22,25 +22,25 @@ if (!all(table_token_tb == token_usdc)) {
 
 # create empty table with token name as column names
 empty_position<- tibble::tibble(!!!setNames(rep(list(logical()), nrow(token_usdc)), token_usdc$V1))
-empty_position <- empty_position %>%
+empty_position <- empty_position |>
   rename_with(~ paste0(.x, "_position"))
 # same as above
 empty_price <- tibble::tibble(!!!setNames(rep(list(logical()), nrow(token_usdc)), token_usdc$V1))
-empty_price <- empty_price %>%
+empty_price <- empty_price |>
   rename_with(~ paste0(.x, "_closing_price"))
 
 
 # bind the two columns time of the two tibbles to get all dates, trades and
 # closing prices
 # add a source column
-trade_df <- all_trade_final %>%
-  mutate(source = "trade") %>%
+trade_df <- all_trade_final |>
+  mutate(source = "trade") |>
   rename(value = time) 
-close_df <- token_daily_close %>%
-  mutate(source = "close") %>%
+close_df <- token_daily_close |>
+  mutate(source = "close") |>
   rename(value = close_time)
 # change TZ for trade_df if we want to keep 00:59:59 for close_df 
-trade_df <- trade_df %>%
+trade_df <- trade_df |>
   mutate(value = as.POSIXct(value, tz = "Europe/Paris"))
 
 # add new columns with "TOKEN_position"
@@ -54,29 +54,34 @@ library(stringr)
 library(tidyr)
 
 # Cr�e une colonne d'ordre
-df <- trade_position %>%
+df <- trade_position |>
   mutate(row_id = row_number())
 
 # On isole les colonnes _position
 position_cols <- names(df)[str_ends(names(df), "_position")]
 
 # Calcul des positions cumul�es par token
-cumulative_positions <- df %>%
-  select(row_id, symbol, executed_qty) %>%
-  filter(!is.na(symbol)) %>%
-  group_by(symbol) %>%
-  mutate(cum_qty = cumsum(executed_qty)) %>%
-  ungroup() %>%
-  mutate(position_col = paste0(symbol, "_position")) %>%
-  select(row_id, position_col, cum_qty) %>%
+cumulative_positions <- df |>
+  select(row_id, symbol, executed_qty) |>
+  filter(!is.na(symbol)) |>
+  group_by(symbol) |>
+  mutate(cum_qty = cumsum(executed_qty)) |>
+  ungroup() |>
+  mutate(position_col = paste0(symbol, "_position")) |>
+  select(row_id, position_col, cum_qty) |>
   pivot_wider(names_from = position_col, values_from = cum_qty)
 
 # Nettoyage et jointure
-df <- df %>%
-  select(-all_of(position_cols)) %>%
-  left_join(cumulative_positions, by = "row_id") %>%
+df <- df |>
+  select(-all_of(position_cols)) |>
+  left_join(cumulative_positions, by = "row_id") |>
   select(-row_id)
 
 # remove lines with source == close
-trade_position_cumul <- df %>%
+trade_position_cumul <- df |>
   filter(source == "trade")
+
+# remove some columns
+trade_position_cumul <- trade_position_cumul |>
+  select(-c('token', 'close', 'row_id'))
+
