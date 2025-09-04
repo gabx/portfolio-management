7f34bd17c5504aab02852b33ed3399d79fd9772a
arnaud gaboury <arnaud.gaboury@gmail.com>
2025-09-04 20:27:40 +0200

    script functions have changed

--- trading_journal_main.r
+++ trading_journal_main.r
@@ -1,16 +1,29 @@
 
+# code is based on work of enrico schuman
+# https://enricoschumann.net/notes/computing-portfolio-pl.html
+# https://enricoschumann.net/notes/valuing-positions.html
+################################################################################
 
 # chosse the correct working directory
 setwd('/development/language/r/portfolio-management')
 
-# load variables, package, settings
-# fetch trades as lists: trade_ls, trade_ls2
-source('environment_variables.r')
+# load package, settings
+# build the portfolio day 1 : trade_tb_start
+source('bootstrap.r')
 
-# fetch all trades
-# result: all_trade_final
-source('get_all_trade.r')
+# create a list or a tibble with our token
+# token_list  <- load_assets("assets.csv", output = "list")
+# token_tib   <- load_assets("assets.csv", output = "tibble")
+source('load_assets.r')
 
-# get daily closing prices @ 00:59:59
-# result: token_daily_close_final
-source('get_daily_closes.r')
+# fetch all trades from day 1, Sys.getenv("START_DATE")
+source('get_all_trades.r')
+
+
+
+
+# get the PL by token
+source('get_pl.r')
+
+# write a table with daily valuation
+source('valuation_table')
